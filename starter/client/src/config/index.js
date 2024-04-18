955800785fdcc960d125ea62fd72b8e77680ea52
smeehui <nguyenvanquanghuy99@gmail.com>
2024-04-18 12:35:48 +0700

    fix auth0 env

--- starter/client/src/config/index.js
+++ starter/client/src/config/index.js
@@ -1,5 +1,6 @@
 export const config = {
     apiId: process.env.REACT_APP_API_ID ?? "4d1ys27dul",
     auth0Domain: process.env.REACT_APP_AUTH0_DOMAIN ?? "dev-g09afexl.us.auth0.com",
+    authClientId: process.env.REACT_APP_AUTH0_CLIENT_ID ?? "z6A6hssGTwrnFnWBwjWNnll7jM6bnx7E",
     s3BucketEndpoint: process.env.REACT_APP_S3_ENDPOINT ?? "https://u-proj-4-todo-app-dev.s3.us-east-1.amazonaws.com"
 }
--- starter/client/src/index.js
+++ starter/client/src/index.js
@@ -1,21 +1,22 @@
-import { Auth0Provider } from '@auth0/auth0-react'
+import {Auth0Provider} from '@auth0/auth0-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import './index.css'
+import {config} from "./config";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const {auth0Domain, authClientId} = config
+
 ReactDOM.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-    audience={`https://${domain}/api/v2/`}
-    scope="read:todo write:todo delete:todo"
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
+    <Auth0Provider
+        domain={auth0Domain}
+        clientId={authClientId}
+        redirectUri={window.location.origin}
+        audience={`https://${auth0Domain}/api/v2/`}
+        scope="read:todo write:todo delete:todo"
+    >
+        <App/>
+    </Auth0Provider>,
+    document.getElementById('root')
 )
